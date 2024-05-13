e4297082f3215a98adbb876ceba5b3f41d337b12
Subhajit Nandi <99741088+SUBHAJITNANDI2002@users.noreply.github.com>
2024-05-13 13:54:37 +0530

    done

---
+++ 04bgChanger/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+ const [color,setColor]=useState("blue")
+
+  return (
+//     <div className="w-full h-screen duration-200 " style={{backgroundColor: color}}>
+// <button>hello</button>
+//     </div>
+<>
+<h1>hello world</h1>
+</>
+  )
+}
+
+export default App
